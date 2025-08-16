29305b78d7f8452745d37850dc3c995c7ff0bb35
Asim Shabbir <ali.haider.subhani2011@gmail.com>
2025-08-16 17:53:25 +0500

    latest code
    Move all data from my inner repository into main

---
+++ Untitled-1.py
@@ -0,0 +1,10 @@
+print("hello wordfhjf")
+print("hello world")
+s=["geeks","for","geeks"]
+print("start here")
+for i in s: 
+    print (i)
+print("mid here")
+for i in s: 
+    print (i)
+print("close here")
